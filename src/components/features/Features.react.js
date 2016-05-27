03afc24cb6db527f334d045dd9ccab8936598da3
alexander <tarasyuk.a@nixsolutions.com>
2016-05-27 11:49:33 +0300

    Change list type

--- src/components/features/Features.react.js
+++ src/components/features/Features.react.js
@@ -4,8 +4,8 @@ import Feature from 'components/feature/Feature.react';
 
 export default ({ features }) => {
   return <div className="features">
-    <ul className="features__list">
+    <ol className="features__list">
       { features.map((feature, index) => <Feature key={ index } { ...feature } />) }
-    </ul>
+    </ol>
   </div>;
 };
