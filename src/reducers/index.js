820931bffe418561b9d83f2b358f27b4e962e983
Alexander T <alexander.v.tarasyuk@gmail.com>
2018-03-21 11:06:13 +0200

    Update babel, webpack, remove useless packages, add badges.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
-import { routerReducer as router } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import features from './features.reducer';
 
 export default combineReducers({
-  router,
   features,
 });
--- webpack/config.js
+++ webpack/config.js
@@ -1,19 +1,19 @@
-import path from 'path';
 import webpack from 'webpack';
+import path from 'path';
 
 export const ROOT_PATH = path.join(__dirname, '..');
-export const APP_PATH  = `${ ROOT_PATH }/src`;
+export const APP_PATH  = path.join(ROOT_PATH, 'src');
 
 export const CONFIG = {
   target: 'web',
   entry: [
-    path.join(APP_PATH, 'main'),
+    path.join(APP_PATH, 'index'),
   ],
 
   module: {
     rules: [
-      { test: /\.jsx?$/, loader: 'eslint-loader', enforce: 'pre', exclude: /node_modules/ },
-      { test: /\.jsx?$/, use: ['thread-loader', 'babel-loader'], exclude: /node_modules/ }
+      { test: /\.jsx?$/, loader: 'eslint-loader', enforce: 'pre', include: APP_PATH },
+      { test: /\.jsx?$/, loader: 'babel-loader', include: APP_PATH },
     ]
   },
 
